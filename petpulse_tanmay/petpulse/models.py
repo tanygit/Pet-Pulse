bd2a31dcc38a435222cd28d664f29851269c9bf9
Tanmay M S <93830435+tanygit@users.noreply.github.com>
2024-06-04 17:58:33 +0530

    linked db file

--- petpulse_tanmay/petpulse/models.py
+++ petpulse_tanmay/petpulse/models.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def get_db_connection():
-    conn = sqlite3.connect('petpulse.db')
+    conn = sqlite3.connect('petpulse_tanmay/petpulse/petpulse.db')
     conn.row_factory = sqlite3.Row
     return conn
 
